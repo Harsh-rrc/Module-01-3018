f2ff98d8641f191a42962e4e1a801008153bb3d8
harsh pandya <hpandya38@rrc.ca>
2025-09-12 21:46:54 -0500

    Implement health check endpoint andchanges all the things.

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
 import express from 'express';
 
+// API version
 const API_VERSION = '1.0.0';
-
 const app = express();
+
+// Middleware to parse JSON requests
 app.use(express.json());
 
-// Common route for testing
+// Basic route for testing
 app.get('/', (req, res) => {
   res.json({ message: 'Server is running!' });
 });
 
-// Health check End-points.
-
+// Health check requirement
 app.get('/api/v1/health', (_req, res) => {
   res.json({
     status: "OK",
